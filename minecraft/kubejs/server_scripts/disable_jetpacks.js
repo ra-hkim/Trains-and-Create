e76d608784eacd1fc86e6ca420acdb14f987b760
rahkim <rahkimali2009@icloud.com>
2026-05-17 19:35:07 +0100

    changed netherite backpack recepie

---
+++ minecraft/kubejs/server_scripts/disable_jetpacks.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes((event) => {
+  event.remove({ output: "create_sa:netherite_jetpack_chestplate" })
+  event.remove({ output: "create_sa:flamethrower"})
+})
---
+++ minecraft/kubejs/server_scripts/jetpack_recipies.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes((event) => {
+  event.smithing(
+    'create_sa:netherite_jetpack_chestplate',
+    'create:brass_block',
+    'create_sa:brass_jetpack_chestplate',
+    'minecraft:netherite_block'
+  )
+})
